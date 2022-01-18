3ab84325392dbee8fc6c6f8795c8ce20e04bb215
Jackson <jacksonmagalhaes@hotmail.com>
2022-01-18 02:04:30 -0300

    Página de login feita

--- pages/login.php
+++ pages/login.php
@@ -10,23 +10,34 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../public/css/global.css">
+  <link rel="stylesheet" href="../public/css/user/login-register.css">
+  <link rel="stylesheet" href="../public/css/user/login.css">
   <title>Login</title>
 </head>
 
 <body>
   <div class="container">
     <div class="center">
-    <h1>Register</h1>
-      <form action="../app/user/login" method="POST">
+      <div>
+        <h1>Register</h1>
+        <h2>Log in to your account</h2>
+      </div>
+      <form action="../app/user/login" method="POST" autocomplete="off">
         <div class="input_content">
+          <input type="text" name="login" required>
+          <span></span>
           <label for="login">Login</label>
-          <input type="text" name="login">
         </div>
         <div class="input_content">
+          <input type="password" name="password" required>
+          <span></span>
           <label for="password">Password</label>
-          <input type="password" name="password">
         </div>
         <button type="submit" name="btn-invite">Enviar</button>
+        <div class="links">
+          <span>Forgot your<a href="">Password</a></span>
+          <span>Don't have an account?<a href="register">Sign In</a></span>
+        </div>
       </form>
     </div>
   </div>

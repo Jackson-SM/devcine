05419dd056383ece940fc5f6c26edcaafe008337
Jackson <jacksonmagalhaes@hotmail.com>
2022-05-19 19:28:38 -0300

    steps form complete

--- public/js/upload/next_prev.js
+++ public/js/upload/next_prev.js
@@ -3,6 +3,13 @@ const prev_btn = document.querySelectorAll(".prev");
 const card_form = document.querySelector(".card_form form");
 const inputs = document.querySelectorAll(".card_form form input");
 
+const pages = document.querySelectorAll(".page");
+
+const steps = document.querySelector(".steps");
+const step = document.querySelectorAll(".steps .step");
+
+let count_btns = 0;
+
 inputs.forEach(element => {
   element.addEventListener('input', event => {
     console.log(event.target.value);
@@ -13,6 +20,13 @@ next_btn.forEach(element => {
   element.addEventListener("click", event => {
     event.preventDefault();
     card_form.scrollBy(300, 0);
+    
+    count_btns += 1;
+
+    console.log(step[count_btns]);
+    console.log(next_btn[count_btns])
+
+    step[count_btns - 1].classList.add("complete");
   })
 });
 
@@ -20,6 +34,12 @@ prev_btn.forEach(element => {
   element.addEventListener("click", event => {
     event.preventDefault();
     card_form.scrollBy(-300, 0);
+
+    count_btns -= 1;
+
+    console.log(step[count_btns]);
+    console.log(next_btn[count_btns])
+
   });
 });
 
